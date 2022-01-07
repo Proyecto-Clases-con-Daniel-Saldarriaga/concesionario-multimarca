8f6fe1fda509da293dbbbc4b50b4f2c472f5a4b8
ismaelc511 <ismaelcarvajal@outlook.com>
2022-01-07 17:20:09 -0500

    Agrego Router generico con Layout

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import PublicLayout from "layouts/PublicLayout";
+import PrivateLayout from "layouts/PrivateLayout";
+import AuthLayout from "layouts/AuthLayout";
+import Index from 'pages/Admin/Index';
+import Vehiculos from 'pages/Admin/Vehiculos';
+import Clientes from 'pages/Admin/Clientes';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import 'styles/styles.css';
+
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<AuthLayout><div>Login</div></AuthLayout>} />
+          <Route path="/registro" element={<AuthLayout><div>Registro</div></AuthLayout>} />
+          <Route path="/admin/index" element={<PrivateLayout><div>Admin</div></PrivateLayout>} />
+          <Route path="/admin/clientes" element={<PrivateLayout><div><Clientes/></div></PrivateLayout>} />
+          <Route path="/admin/vehiculos" element={<PrivateLayout><div><Vehiculos/></div></PrivateLayout>} />
+          <Route path="/" element={<PublicLayout><div>Index</div></PublicLayout>} />
+        </Routes>
+      </BrowserRouter>
+
+    </>
+  );
+}
+
+export default App;
---
+++ src/layouts/PublicLayout.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from 'components/Navbar'
+import Footer from 'components/Footer'
+
+const PublicLayout = ({ children }) => {
+    return (
+    <div className='flex flex-col justify-between h-screen'>
+        <Navbar />
+        <main className='h-full overflow-y-scroll bg-red-300'>{ children }</main>
+        <Footer />
+    </div>
+    );
+}
+
+export default PublicLayout
